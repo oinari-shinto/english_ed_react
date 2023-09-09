a56670914671d05912d3cdfd1d487170fe0061d2
oinari-shinto <oinari.shinto@gmail.com>
2023-09-09 15:23:15 +0300

    start edit App jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import styles from "./style";
 
 const App = () => (
   <div className='bg-primary w-full overflow-hidden'>
-    <h1>Hello world</h1>
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        Navbar
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}}`}>
+
+    </div>
 
   </div>
   );  
